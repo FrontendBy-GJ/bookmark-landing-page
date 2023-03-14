cb894107e519092ae73af7465b6ec6d823ce205c
Genaro Garcia <genaro.dev@outlook.com>
2023-03-14 12:49:47 -0500

    style: adjust text color, button color, add tab index to buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,14 @@ import hero from '../assets/illustration-hero.svg';
 
 export default function Hero() {
   return (
-    <div className="hero lg:relative overflow-hidden max-w-[1440px] mx-auto z-0 prose">
+    <div className="hero lg:relative overflow-hidden max-w-[1440px] mx-auto z-0 prose prose-headings:text-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse text-center lg:text-left lg:px-10">
         <div className="relative mb-5 lg:mb-0 lg:static">
           <div className="h-3/4 lg:h-2/3 bg-primary rounded-l-full absolute bottom-0 -right-16 md:-right-28 lg:right-0 lg:w-2/5 w-full -z-10"></div>
-          <img src={hero} alt="" className="z-50" />
+          <img src={hero} alt="hero image" className="z-50" />
         </div>
         <div className="max-w-lg z-50 px-1">
-          <h1 className="">
+          <h1>
             A Simple Bookmark <br className="hidden lg:block" /> Manager
           </h1>
           <p className="text-neutral">
@@ -19,10 +19,13 @@ export default function Hero() {
             free.
           </p>
           <div className="space-x-5 flex">
-            <button className="btn bg-primary border-none shadow-lg">
+            <button tabIndex={0} className="btn bg-primary shadow-lg">
               Get it on Chrome
             </button>
-            <button className="btn bg-slate-100 text-neutral border-none shadow-lg hover:text-white">
+            <button
+              tabIndex={0}
+              className="btn bg-slate-100 text-neutral shadow-lg hover:text-neutral hover:border-neutral"
+            >
               Get it on Firefox
             </button>
           </div>
